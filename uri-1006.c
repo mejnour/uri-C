150ec0886f3e7ab21fa5c264254443b7ffa9efae
mejnour <apina.dev@gmail.com>
2018-07-29 12:41:55 -0300

    Average 2

---
+++ uri-1006.c
@@ -0,0 +1,15 @@
+// Average 2
+
+#include <stdio.h>
+
+int main() {
+  float a, b, c, media;
+
+  scanf("%f %f %f", &a, &b, &c);
+
+  media = (a * 2 + b * 3 + c * 5)/(2 + 3 + 5);
+
+  printf("MEDIA = %.1f\n", media);
+
+  return 0;
+}
