e3f549ef92db1759c8e0b117c85bb94503aa98e9
mejnour <apina.dev@gmail.com>
2018-08-02 21:18:18 -0300

    Distance Between Two Points

---
+++ uri-1015.c
@@ -0,0 +1,12 @@
+// Distance Between Two Points
+
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+  double x1, y1, x2, y2;
+
+  scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+
+  printf("%.4lf\n", sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)));
+}
